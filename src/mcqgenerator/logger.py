1721c01464d220528c0e7e6fe547a3bbaa4b7570
Bobby <manchanavyatej@gmail.com>
2026-05-18 16:38:38 +0530

    i created a logger

--- src/mcqgenerator/logger.py
+++ src/mcqgenerator/logger.py
@@ -1,3 +1,14 @@
 import logging 
 import os
 from datetime import datetime
+
+LOG_FILE=f"mcq_generator_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+
+log_path=os.path.join(os.getcwd(), "logs", LOG_FILE)
+
+os.makedirs(os.path.dirname(log_path), exist_ok=True)
+
+LOG_FORMAT=os.path.join(log_path,LOG_FILE)
+
+logging.basicConfig(level=logging.INFO,filename=log_path, format="[%(asctime)s] %(levelname)s - %(message)s", force=True)
+
---
+++ test.py
@@ -0,0 +1,3 @@
+from src.mcqgenerator.logger import logging
+
+logging.info("Iam going to start my execution....")
